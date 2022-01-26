f7e5c012f9776db9f8aadf42052a5b5f56e109f9
Amaury Vanoorenberghe <amaury.vanoorenberghe.etu@univ-lille.fr>
2022-01-26 16:36:50 +0100

    Modification du main du webserver pour accepter les connexions entrantes et envoyer un message de bienvenue aux clients (chap 3)

--- webserver/main.c
+++ webserver/main.c
@@ -1,14 +1,34 @@
+#include <errno.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <unistd.h>
+
+#include "socket.h"
+
+#define UNUSED(x) (void)(x)
 
 int main(int argc, char **argv) {
-    /* Arnold Robbins in the LJ of February '95, describing RCS */
-    
-    if (argc > 1 && strcmp(argv[1], "-advice") == 0) {
-        printf("Don't Panic!\n");
-        return 42;
+    UNUSED(argc);
+    UNUSED(argv);
+
+    int socket_serveur = creer_serveur(8080);
+
+    while (1) {
+        int socket_client = accept(socket_serveur, NULL, NULL);
+        if (socket_client == -1) {
+            perror("accept client_socket");
+            continue;
+        }
+
+        const char *message_bienvenue = "Bonjour, bienvenue sur mon serveur\n";
+        write(socket_client, message_bienvenue, strlen(message_bienvenue));
+
+        // fflush(socket_client) ???
+        // close(socket_client) ??? [+ perror]
     }
 
-    printf("Need an advice?\n");
-    return 0;
+    return EXIT_SUCCESS;
 }
