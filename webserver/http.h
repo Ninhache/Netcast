977dc731362bbacddce91f7c8254b079ae6d9dda
Néo Almeida <neo.almeida.etu@univ-lille.fr>
2022-03-11 22:53:35 +0100

    Ajout de doc manquante

--- webserver/http.h
+++ webserver/http.h
@@ -5,4 +5,10 @@
 #define BUFFER_SIZE 1024
 #define HTTP_LINE_LENGTH 8192
 
+/**
+ * @brief Permet de passer le header d'un client.
+ * @param io_client Le client en question
+ */
+void skip_header(FILE* io_client);
+
 #endif
